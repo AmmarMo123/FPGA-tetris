2092da3451cc2dad26f11464b75bb638c79a25bb
AmmarMo123 <ammarmo519@gmail.com>
2024-11-23 14:25:46 -0500

    Fix square module

--- vga_square_habr.v
+++ vga_square_habr.v
@@ -1,20 +1,21 @@
 module tvPattern(
-input [9:0] x,
-input [9:0] y,
-output reg [3:0] red,
-output reg [3:0] green,
-output reg [3:0] blue);
+    input [9:0] x,
+    input [9:0] y,
+    output reg [3:0] red,
+    output reg [3:0] green,
+    output reg [3:0] blue
+);
 
-always @(x||y)
-begin
-if(x>=0 && x<100 && y>=0 && y<100)) 
-	begin
-		red <= 4'hF;
-		green <= 4'hF;
-		blue <= 4'hF;
-	end
+always @(x or y) begin
+    if (x < 100 && y < 100) begin
+        red = 4'hF;
+        green = 4'hF;
+        blue = 4'hF;
+    end else begin
+        red = 4'h0;
+        green = 4'h0;
+        blue = 4'h0;
+    end
 end
-
-
 
 endmodule
