c5859269489b787a54d86c07a5aa55e60f2100b3
AmmarMo123 <ammarmo519@gmail.com>
2024-11-23 20:22:18 -0500

    Add tetris drawing pattern, clean up

--- pattern_generator.v
+++ pattern_generator.v
@@ -1,97 +1,157 @@
 module pattern_generator (
-    input wire [9:0] counter_x,    // Horizontal counter
-    input wire [9:0] counter_y,    // Vertical counter
-    output reg [3:0] r_red,        // Red output
-    output reg [3:0] r_green,      // Green output
-    output reg [3:0] r_blue        // Blue output
+	input clk,
+    input rst,
+    input [9:0] counter_x,        // Horizontal counter
+    input [9:0] counter_y,        // Vertical counter
+	input [239:0] data,           // Input grid
+    output reg [3:0] r_red,       // Red output
+    output reg [3:0] r_green,     // Green output
+    output reg [3:0] r_blue       // Blue output
 );
 
-    always @ (*) begin
-        // Default color values
-        r_red = 4'h0;
-        r_green = 4'h0;
-        r_blue = 4'h0;
+    // Constants to define Tetris grid
+    parameter COL_START = 340;
+    parameter ROW_START = 67;
+    parameter BLOCK_SIZE = 17;
+    parameter GAP = 4;
+    reg [4:0] game_area_row;
+    reg valid_line;
 
-        if (counter_y < 135) begin              
-            r_red = 4'hF;    // White
-            r_blue = 4'hF;
-            r_green = 4'hF;
-        end else if (counter_y >= 135 && counter_y < 205) begin
-            if (counter_x < 324) begin 
-                r_red = 4'hF;    // White
-                r_blue = 4'hF;
-                r_green = 4'hF;
-            end else if (counter_x >= 324 && counter_x < 604) begin 
-                r_red = 4'hF;    // Yellow
-                r_blue = 4'h0;
-                r_green = 4'hF;
-            end else if (counter_x >= 604) begin 
-                r_red = 4'hF;    // White
-                r_blue = 4'hF;
-                r_green = 4'hF;
-            end
-        end else if (counter_y >= 205 && counter_y < 217) begin
-            if (counter_x < 324) begin 
-                r_red = 4'hF;    // White
-                r_blue = 4'hF;
-                r_green = 4'hF;
-            end else if (counter_x >= 324 && counter_x < 371) begin 
-                r_red = 4'hF;    // Yellow
-                r_blue = 4'h0;
-                r_green = 4'hF;
-            end else if (counter_x >= 371 && counter_x < 383) begin 
-                r_red = 4'h0;    // Black
-                r_blue = 4'h0;
-                r_green = 4'h0;
-            end else if (counter_x >= 383 && counter_x < 545) begin 
-                r_red = 4'hF;    // Yellow
-                r_blue = 4'h0;
-                r_green = 4'hF;
-            end else if (counter_x >= 545 && counter_x < 557) begin 
-                r_red = 4'h0;    // Black
-                r_blue = 4'h0;
-                r_green = 4'h0;
-            end else if (counter_x >= 557 && counter_x < 604) begin 
-                r_red = 4'hF;    // Yellow
-                r_blue = 4'h0;
-                r_green = 4'hF;
-            end else if (counter_x >= 604) begin 
-                r_red = 4'hF;    // White
-                r_blue = 4'hF;
-                r_green = 4'hF;
-            end
-        end else if (counter_y >= 217 && counter_y < 305) begin
-            if (counter_x < 324) begin 
-                r_red = 4'hF;    // White
-                r_blue = 4'hF;
-                r_green = 4'hF;
-            end else if (counter_x >= 324 && counter_x < 604) begin 
-                r_red = 4'hF;    // Yellow
-                r_blue = 4'h0;
-                r_green = 4'hF;
-            end else if (counter_x >= 604) begin 
-                r_red = 4'hF;    // White
-                r_blue = 4'hF;
-                r_green = 4'hF;
-            end
-        end else if (counter_y >= 305 && counter_y < 414) begin
-            if (counter_x < 324) begin 
-                r_red = 4'hF;    // White
-                r_blue = 4'hF;
-                r_green = 4'hF;
-            end else if (counter_x >= 324 && counter_x < 604) begin 
-                r_red = 4'hF;    // Yellow
-                r_blue = 4'h0;
-                r_green = 4'hF;
-            end else if (counter_x >= 604) begin 
-                r_red = 4'hF;    // White
-                r_blue = 4'hF;
-                r_green = 4'hF;
-            end
-        end else if (counter_y >= 414) begin              
-            r_red = 4'hF;    // White
-            r_blue = 4'hF;
-            r_green = 4'hF;
+    // Determine which rows to draw the blocks in
+    always @ (posedge clk)
+    begin
+        if(rst) begin
+            valid_line <= 0;
+            game_area_row <= 0;
+        end else begin
+            if((counter_y >= ROW_START) && (counter_y <= ROW_START + BLOCK_SIZE)) begin
+            game_area_row <= 0;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + BLOCK_SIZE + GAP) && (counter_y <= ROW_START + 2*BLOCK_SIZE + GAP)) begin
+            game_area_row <= 1;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 2*BLOCK_SIZE + 2*GAP) && (counter_y<= ROW_START + 3*BLOCK_SIZE + 2*GAP)) begin
+            game_area_row <= 2;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 3*BLOCK_SIZE + 3*GAP) && (counter_y <= ROW_START + 4*BLOCK_SIZE + 3*GAP)) begin
+            game_area_row <= 3;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 4*BLOCK_SIZE + 4*GAP) && (counter_y <= ROW_START + 5*BLOCK_SIZE + 4*GAP)) begin
+            game_area_row <= 4;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 5*BLOCK_SIZE + 5*GAP) && (counter_y <= ROW_START + 6*BLOCK_SIZE + 5*GAP)) begin
+            game_area_row <= 5;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 6*BLOCK_SIZE + 6*GAP) && (counter_y <= ROW_START + 7*BLOCK_SIZE + 6*GAP)) begin
+            game_area_row <= 6;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 7*BLOCK_SIZE + 7*GAP) && (counter_y <= ROW_START + 8*BLOCK_SIZE + 7*GAP)) begin
+            game_area_row <= 7;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 8*BLOCK_SIZE + 8*GAP) && (counter_y <= ROW_START + 9*BLOCK_SIZE + 8*GAP)) begin
+            game_area_row <= 8;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 9*BLOCK_SIZE + 9*GAP) && (counter_y <= ROW_START + 10*BLOCK_SIZE + 9*GAP)) begin
+            game_area_row <= 9;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 10*BLOCK_SIZE + 10*GAP) && (counter_y <= ROW_START + 11*BLOCK_SIZE + 10*GAP)) begin
+            game_area_row <= 10;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 11*BLOCK_SIZE + 11*GAP) && (counter_y <= ROW_START + 12*BLOCK_SIZE + 11*GAP)) begin
+            game_area_row <= 11;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 12*BLOCK_SIZE + 12*GAP) && (counter_y <= ROW_START + 13*BLOCK_SIZE + 12*GAP)) begin
+            game_area_row <= 12;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 13*BLOCK_SIZE + 13*GAP) && (counter_y <= ROW_START + 14*BLOCK_SIZE + 13*GAP)) begin
+            game_area_row <= 13;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 14*BLOCK_SIZE + 14*GAP) && (counter_y <= ROW_START + 15*BLOCK_SIZE + 14*GAP)) begin
+            game_area_row <= 14;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 15*BLOCK_SIZE + 15*GAP) && (counter_y <= ROW_START + 16*BLOCK_SIZE + 15*GAP)) begin
+            game_area_row <= 15;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 16*BLOCK_SIZE + 16*GAP) && (counter_y <= ROW_START + 17*BLOCK_SIZE + 16*GAP)) begin
+            game_area_row <= 16;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 17*BLOCK_SIZE + 17*GAP) && (counter_y <= ROW_START + 18*BLOCK_SIZE + 17*GAP)) begin
+            game_area_row <= 17;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 18*BLOCK_SIZE + 18*GAP) && (counter_y <= ROW_START + 19*BLOCK_SIZE + 18*GAP)) begin
+            game_area_row <= 18;
+            valid_line <= 1;
+            end else if((counter_y >= ROW_START + 19*BLOCK_SIZE + 19*GAP) && (counter_y <= ROW_START + 20*BLOCK_SIZE + 19*GAP)) begin
+            game_area_row <= 19;
+            valid_line <= 1;
+            end else
+            valid_line <= 0;
         end
-    end
+    end  // 1st always
+
+    // Exctact the given row
+    wire [11:0] game_area_mx;
+    assign game_area_mx = valid_line ? data[(game_area_row)*12 : (game_area_row)*12 + 12] : 12'h000;
+
+    // Determine the columns and draw the blocks
+    always @ (posedge clk)
+    begin
+        if(rst) begin
+            r_red   <= 4'h0;
+            r_blue  <= 4'h0;
+            r_green <= 4'h0;
+        end else begin
+            // Game stage
+            if((game_area_mx[0] == 1) && ((counter_x >= COL_START) && (counter_x <= COL_START + BLOCK_SIZE))) begin
+            r_red   <= 4'hF;
+            r_blue  <= 4'hF;
+            r_green <= 4'hF;
+            end else if((game_area_mx[1] == 1) && ((counter_x >= COL_START + BLOCK_SIZE + GAP) && (counter_x <= COL_START + 2*BLOCK_SIZE + GAP))) begin
+            r_red   <= 4'hF;
+            r_blue  <= 4'hF;
+            r_green <= 4'hF;
+            end else if((game_area_mx[2] == 1) && ((counter_x >= COL_START + 2*BLOCK_SIZE + 2*GAP) && (counter_x <= COL_START + 3*BLOCK_SIZE + 2*GAP))) begin
+            r_red   <= 4'hF;
+            r_blue  <= 4'hF;
+            r_green <= 4'hF;
+            end else if((game_area_mx[3] == 1) && ((counter_x >= COL_START + 3*BLOCK_SIZE + 3*GAP) && (counter_x <= COL_START + 4*BLOCK_SIZE + 3*GAP)))) begin
+            r_red   <= 4'hF;
+            r_blue  <= 4'hF;
+            r_green <= 4'hF;
+            end else if((game_area_mx[4] == 1) && ((counter_x >= COL_START + 4*BLOCK_SIZE + 4*GAP) && (counter_x <= COL_START + 5*BLOCK_SIZE + 4*GAP))) begin
+            r_red   <= 4'hF;
+            r_blue  <= 4'hF;
+            r_green <= 4'hF;
+            end else if((game_area_mx[5] == 1) && ((counter_x >= COL_START + 5*BLOCK_SIZE + 5*GAP) && (counter_x <= COL_START + 6*BLOCK_SIZE + 5*GAP))) begin
+            r_red   <= 4'hF;
+            r_blue  <= 4'hF;
+            r_green <= 4'hF;
+            end else if((game_area_mx[6] == 1) && ((counter_x >= COL_START + 6*BLOCK_SIZE + 6*GAP) && (counter_x <= COL_START + 7*BLOCK_SIZE + 6*GAP))) begin
+            r_red   <= 4'hF;
+            r_blue  <= 4'hF;
+            r_green <= 4'hF;
+            end else if((game_area_mx[7] == 1) && ((counter_x >= COL_START + 7*BLOCK_SIZE + 7*GAP) && (counter_x <= COL_START + 8*BLOCK_SIZE + 7*GAP))) begin
+            r_red   <= 4'hF;
+            r_blue  <= 4'hF;
+            r_green <= 4'hF;
+            end else if((game_area_mx[8] == 1) && ((counter_x >= COL_START + 8*BLOCK_SIZE + 8*GAP) && (counter_x <= COL_START + 9*BLOCK_SIZE + 8*GAP))) begin
+            r_red   <= 4'hF;
+            r_blue  <= 4'hF;
+            r_green <= 4'hF;
+            end else if((game_area_mx[9] == 1) && ((counter_x >= COL_START + 9*BLOCK_SIZE + 9*GAP) && (counter_x <= COL_START + 10*BLOCK_SIZE + 9*GAP))) begin
+            r_red   <= 4'hF;
+            r_blue  <= 4'hF;
+            r_green <= 4'hF;
+            end else if((game_area_mx[10] == 1) && ((counter_x >= COL_START + 10*BLOCK_SIZE + 10*GAP) && (counter_x <= COL_START + 11*BLOCK_SIZE + 10*GAP))) begin
+            r_red   <= 4'hF;
+            r_blue  <= 4'hF;
+            r_green <= 4'hF;
+            end else if((game_area_mx[11] == 1) && ((counter_x >= COL_START + 11*BLOCK_SIZE + 11*GAP) && (counter_x <= COL_START + 12*BLOCK_SIZE + 11*GAP))) begin
+            r_red   <= 4'hF;
+            r_blue  <= 4'hF;
+            r_green <= 4'hF;
+            end
+        end
+    end // 2nd always
+						
 endmodule
---
+++ test.v
@@ -0,0 +1,113 @@
+module test(
+   input clock50MHz,
+   output [3:0] red,
+   output [3:0] blue,
+   output [3:0] green,
+   output hsync,
+   output vsync,
+);
+	reg [9:0] counter_x = 0;  // horizontal counter
+	reg [9:0] counter_y = 0;  // vertical counter
+	wire [3:0] r_red;
+	wire [3:0] r_blue;
+	wire [3:0] r_green;
+	
+	reg reset = 0;  // for PLL
+	
+	wire clk25MHz;
+	
+	wire [239:0] array; // 20x12 array
+	//assign array[0] = 1'b0;
+
+    /* Top left corners
+    assign array = {
+        12'b100000000001, 12'b000000000000, 12'b000000000000, 12'b000000000000, 
+        12'b000000000000, 12'b000000000000, 12'b000000000000, 12'b000000000000, 
+        12'b000000000000, 12'b000000000000, 12'b000000000000, 12'b000000000000, 
+        12'b000000000000, 12'b000000000000, 12'b000000000000, 12'b000000000000, 
+        12'b000000000000, 12'b000000000000, 12'b000000000000, 12'b100000000001
+    };
+    */
+
+    /* full rows
+    assign array = {
+        12'b111111111111, 12'b000000000000, 12'b000000000000, 12'b000000000000, 
+        12'b000000000000, 12'b000000000000, 12'b000000000000, 12'b000000000000, 
+        12'b000000000000, 12'b111111111111, 12'b000000000000, 12'b000000000000, 
+        12'b000000000000, 12'b000000000000, 12'b000000000000, 12'b000000000000, 
+        12'b000000000000, 12'b111111111111, 12'b111111111111, 12'b111111111111
+    };
+    */
+
+    /* square
+    assign array = {
+        12'b000000000000, 12'b000000000000, 12'b000000000000, 12'b000000000000, 
+        12'b000000000000, 12'b000000000000, 12'b000000000000, 12'b000000000000, 
+        12'b000000000000, 12'b000000000000, 12'b000000000000, 12'b000000000000, 
+        12'b000000000000, 12'b000000000000, 12'b000000000000, 12'b000000000000, 
+        12'b000001100000, 12'b000011110000, 12'b001111111100, 12'b111111111111
+    };
+    */
+
+    /* Final test
+    assign array = {
+        12'b110011001100, 12'b101010101010, 12'b000000000000, 12'b111111111111, 
+        12'b000011111111, 12'b111100001111, 12'b110011001100, 12'b101010101010, 
+        12'b000000000000, 12'b111111111111, 12'b000011111111, 12'b111100001111, 
+        12'b110011001100, 12'b101010101010, 12'b000000000000, 12'b111111111111, 
+        12'b000011111111, 12'b111100001111, 12'b110011001100, 12'b101010101010
+    };
+    */
+
+	// clk divider 50 MHz to 25 MHz
+	ip ip1(
+		.areset(reset),
+		.inclk0(clock50MHz),
+		.c0(clk25MHz),
+		.locked()
+		);  
+
+	// counter and sync generation
+	always @(posedge clk25MHz)  // horizontal counter
+		begin 
+			if (counter_x < 799)
+				counter_x <= counter_x + 1;  // horizontal counter (including off-screen horizontal 160 pixels) total of 800 pixels 
+			else
+				counter_x <= 0;              
+		end  // always 
+	
+	always @ (posedge clk25MHz)  // vertical counter
+		begin 
+			if (counter_x == 799)  // only counts up 1 count after horizontal finishes 800 counts
+				begin
+					if (counter_y < 525)  // vertical counter (including off-screen vertical 45 pixels) total of 525 pixels
+						counter_y <= counter_y + 1;
+					else
+						counter_y <= 0;              
+				end 
+		end
+
+	// hsync and vsync output assignments
+	assign hsync = (counter_x >= 0 && counter_x < 96) ? 1:0;  // hsync high for 96 counts                                                 
+	assign vsync = (counter_y >= 0 && counter_y < 2) ? 1:0;   // vsync high for 2 counts
+
+	// pattern generator
+	pattern_generator pattern_gen(
+		.clk(clock50MHz),
+        .rst(reset)
+		.counter_x(counter_x),
+		.counter_y(counter_y),
+		.data(array),
+		.r_red(r_red),
+		.r_green(r_green),
+		.r_blue(r_blue)
+	);			
+
+	// color output assignments
+	// only output the colors if the counters are within the adressable video time constraints
+	assign red = (counter_x > 144 && counter_x <= 787 && counter_y > 35 && counter_y <= 514) ? r_red : 4'h0;
+	assign blue = (counter_x > 144 && counter_x <= 787 && counter_y > 35 && counter_y <= 514) ? r_blue : 4'h0;
+	assign green = (counter_x > 144 && counter_x <= 787 && counter_y > 35 && counter_y <= 514) ? r_green : 4'h0;
+	// end color output assignments
+
+endmodule
